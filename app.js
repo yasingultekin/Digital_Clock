7ceefd4483e4670f48e8e7d33330da0dc6f21804
yasingultekin <yasingultekin13@gmail.com>
2022-06-21 15:22:40 +0300

    js updated.

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const clock = document.querySelector(".clock");
 function showTime() {
     let dateTime = new Date();
     //   console.log(dateTime);
+
+    let time = dateTime.toLocaleTimeString();
+    //   console.log(time);
 }
