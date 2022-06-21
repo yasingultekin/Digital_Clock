308e07cd0b1f134ed66ca72603fce19cfc0b4886
yasingultekin <yasingultekin13@gmail.com>
2022-06-21 15:28:43 +0300

    js updated.

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ function showTime() {
 
     let hours = dateTime.getHours();
     //   console.log(hours);
+
+    if (0 > hours < 13 && hours < 13) {
+        clock.innerHTML += `<span class="span">AM</span>`;
+    } else {
+        clock.innerHTML += `<span class="span">PM</span>`;
+    }
 }
 
 setInterval(showTime, 1000);
